8d5a877b4a3b73f1dcf0745f1f1de760f8971db0
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-04-25 18:13:35 -0300

    restTemplate

--- hr-payroll/src/main/java/com/spiet/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/spiet/hrpayroll/services/PaymentService.java
@@ -1,12 +1,32 @@
 package com.spiet.hrpayroll.services;
 
 import com.spiet.hrpayroll.entities.Payment;
+import com.spiet.hrpayroll.entities.Worker;
+import org.hibernate.jdbc.Work;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, int days) {
-        return Payment.builder().name("Bob").dailyIncome(200.0).days(days).build();
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/{id}", Worker.class,
+                uriVariables);
+
+        return Payment.builder().name(worker.getName()).dailyIncome(worker.getDailyIncome()).days(days).build();
     }
 }
