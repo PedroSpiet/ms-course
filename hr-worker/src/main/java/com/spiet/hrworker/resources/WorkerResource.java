fe829d444fb177a8dc42a2448fd2ec7711368998
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-04-25 18:59:37 -0300

    Balanceamento de carga com Ribbon

--- hr-worker/src/main/java/com/spiet/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/spiet/hrworker/resources/WorkerResource.java
@@ -3,7 +3,10 @@ package com.spiet.hrworker.resources;
 import com.spiet.hrworker.entities.Worker;
 import com.spiet.hrworker.repositories.WorkerRepository;
 import org.hibernate.jdbc.Work;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +15,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/workers")
 public class WorkerResource {
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkerRepository repo;
@@ -24,6 +30,7 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("PORT: " + env.getProperty("local.server.port"));
         Worker worker = repo.findById(id).get();
         return ResponseEntity.ok().body(worker);
     }
