7e239554e3ab5c55e3cb6131b6b2e541905effc5
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-04-25 21:15:47 -0300

    add hr-user

---
+++ hr-user/src/main/java/com/spiet/hruser/HrUserApplication.java
@@ -0,0 +1,14 @@
+package com.spiet.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class HrUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrUserApplication.class, args);
+    }
+
+}
