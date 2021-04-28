7e029851a8bd5f5bfdaf01594062d44a0b18213d
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-04-27 22:01:03 -0300

    authentication oauth jwt

--- hr-oauth/src/main/java/com/spiet/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/spiet/hroauth/entities/User.java
@@ -2,17 +2,22 @@ package com.spiet.hroauth.entities;
 
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 
     private static final long serialVersionUID = 8833921007606169182L;
 
@@ -25,4 +30,39 @@ public class User implements Serializable {
     private String password;
 
    private Set<Role> role = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
